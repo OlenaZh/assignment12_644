097efba1c98f35c60be52262572768f65422f630
OlenaZh <alyona.mesh@gmail.com>
2022-06-01 01:28:41 -0700

    oop - commented

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -94,7 +94,7 @@ console.log('The ' + pet.speak() + ' has made a noise!');
 */
 
 // STEP 9
-String.prototype.numWords = function (findWords) {
+/*String.prototype.numWords = function (findWords) {
     let haystack = [], total = 0, needle;
     haystack = this.split(' ');
     for (needle = 0; needle < haystack.length; needle++) {
@@ -105,4 +105,4 @@ String.prototype.numWords = function (findWords) {
     return total;
 };
 let message = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
-console.log(message.numWords('lorem'));
+console.log(message.numWords('lorem'));*/
